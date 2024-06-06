93281e526dff49f434b59557eb50f1c8ff4cff3d
uc <woo8618@gmail.com>
2024-06-06 15:28:35 +0900

    Mybatis_practice

--- src/main/java/com/ex/controller/TestController.java
+++ src/main/java/com/ex/controller/TestController.java
@@ -37,7 +37,7 @@ public class TestController {
 	public String insert(MyTestDTO myTestDTO, Model model) {
 		model.addAttribute("result", testService.mytestInsert(myTestDTO));
 		
-		return "insert";
+		return "main";
 	}
 	
 //	로그인 Get
